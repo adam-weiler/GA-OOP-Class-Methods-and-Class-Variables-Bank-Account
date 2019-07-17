27874cb21b3d415546300614d5295f553f9d36c3
adam-weiler <adam.weiler@yahoo.ca>
2019-07-17 13:21:06 -0400

    Creating a file for the first exercise.

---
+++ BankAccount.py
@@ -0,0 +1,15 @@
+class BankAccount():
+
+    interest_rate = .01 #This is a class variable.
+
+
+
+
+    @classmethod #This is a class method.
+    def total_population(cls):
+        # stuff
+
+
+
+account_one = BankAccount()
+print(account_one)
