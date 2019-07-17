f27794390fd5e3b2bca4eebbbcfa558a6990ac70
adam-weiler <adam.weiler@yahoo.ca>
2019-07-17 14:34:33 -0400

    Objects are being created but not available in total.

--- BankAccount.py
+++ BankAccount.py
@@ -5,8 +5,8 @@ class BankAccount():
     accounts = []
 
 
-    def __init__(self):
-        self.balance = 0 #This is an instance variable.
+    def __init__(self, balance=0):
+        self.balance = balance #This is an instance variable.
         # self.account_name = account_name
 
 
@@ -26,35 +26,61 @@ class BankAccount():
 
     @classmethod
     def total_funds(cls):
+        our_total = 0
+        print('\n TOTAL FUNDS')
 
-        for account in cls.accounts:
-            print('hey')
+        # print(len(cls.accounts)) #2
+
+
+        for num in range(len(cls.accounts)):
+            # print(f'--{num}') #0, 1
+            print(cls.accounts[num]) #main BankAccount
+            # print(cls.accounts[num])
+
+        # for account in len(cls.accounts):
+        #     print(type(cls.accounts[0].balance))        
+
+        # for account in cls.accounts:
+            # print(cls.accounts[0].balance)
+            # print(account.balance)
+            # our_total += cls.accounts[account].balance
+        
+        return our_total
         
     
 
+    # @classmethod
+    # def interest_time(cls):
+    #     our_total = 0
 
 
 
 
+print('A')
 my_account = BankAccount.create()
 your_account = BankAccount.create()
 print(my_account.balance) # 0
-print(BankAccount.total_funds()) # 0
+# print(BankAccount.total_funds()) # 0
 print()
 
+print(my_account)
+
+print('B - Balance Balance Total-Funds')
 my_account.deposit(200)
 your_account.deposit(1000)
 print(my_account.balance) # 200
 print(your_account.balance) # 1000
-# print(BankAccount.total_funds()) # 1200
+print(BankAccount.total_funds()) # 1200
 print()
 
+print('C')
 # BankAccount.interest_time()
-print(my_account.balance) # 202.0
-print(your_account.balance) # 1010.0
+# print(my_account.balance) # 202.0
+# print(your_account.balance) # 1010.0
 # print(BankAccount.total_funds()) # 1212.0
 print()
 
+print('D')
 my_account.withdraw(50)
 print(my_account.balance) # 152.0
 # print(BankAccount.total_funds()) # 1162.0
