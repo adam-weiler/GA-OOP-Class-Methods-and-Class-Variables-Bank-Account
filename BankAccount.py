1a680a1df2ddd319c19f93f1d0e28d7c2d11f590
adam-weiler <adam.weiler@yahoo.ca>
2019-07-17 13:45:24 -0400

    Added functionality to create account, withdraw and deposit,

--- BankAccount.py
+++ BankAccount.py
@@ -1,15 +1,60 @@
 class BankAccount():
 
-    interest_rate = .01 #This is a class variable.
+    #These are class variables.
+    interest_rate = .01
+    accounts = []
+
+
+    def __init__(self):
+        self.balance = 0 #This is an instance variable.
+        # self.account_name = account_name
+
+
+    def deposit(self, amount): #This is an instance method.
+        self.balance += amount
+
+
+    def withdraw(self, amount): #This is an instance method.
+        self.balance -= amount
+
+
+    #These are class methods.
+    @classmethod
+    def create(cls):
+        cls.accounts.append(BankAccount())
+        return cls()
+
+    @classmethod
+    def total_funds(cls):
+
+        for account in cls.accounts:
+            print('hey')
+        
+    
 
 
 
 
-    @classmethod #This is a class method.
-    def total_population(cls):
-        # stuff
 
+my_account = BankAccount.create()
+your_account = BankAccount.create()
+print(my_account.balance) # 0
+print(BankAccount.total_funds()) # 0
+print()
 
+my_account.deposit(200)
+your_account.deposit(1000)
+print(my_account.balance) # 200
+print(your_account.balance) # 1000
+# print(BankAccount.total_funds()) # 1200
+print()
 
-account_one = BankAccount()
-print(account_one)
+# BankAccount.interest_time()
+print(my_account.balance) # 202.0
+print(your_account.balance) # 1010.0
+# print(BankAccount.total_funds()) # 1212.0
+print()
+
+my_account.withdraw(50)
+print(my_account.balance) # 152.0
+# print(BankAccount.total_funds()) # 1162.0
